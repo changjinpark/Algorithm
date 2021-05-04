eff7e1834bc39efb0ce19095e7cae32a19f6f4d2
changjin.park <pcjin118@gmail.com>
2021-05-04 23:41:46 +0900

    Inflearn : 2021-05-04

--- Inflearn.com/6-8.py
+++ Inflearn.com/6-8.py
@@ -1,18 +1,22 @@
 def DFS(L):
     global cnt
     if L == m:
-        for i in range(0, m):
+        for i in range(m):
             print(res[i], end=' ')
         print()
         cnt += 1
     else:
         for i in range(1, n+1):
-            res[L] = i
-            DFS(L+1)
+            if ch[i] == 0:
+                ch[i] = 1
+                res[L] = i
+                DFS(L+1)
+                ch[i] = 0
 
 if __name__=="__main__":
-    n, m= map(int, input().split())
+    n, m = map(int, input().split())
     res = [0] * m
     cnt = 0
+    ch=[0]*(n+1)
     DFS(0)
     print(cnt)
