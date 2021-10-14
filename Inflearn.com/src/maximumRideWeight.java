e7e83560203b9453204a3c389e0e293cd9f5ae17
changjin.park <pcjin118@gmail.com>
2021-10-14 23:41:02 +0900

    Inflearn: 최대탑승무게 구하기

---
+++ Inflearn.com/src/maximumRideWeight.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class maximumRideWeight {
+
+    private static int limit, n, answer = Integer.MIN_VALUE;
+    private static int[] dogWeight;
+
+    private static void dfs(int L, int weightSum) {
+        if(weightSum > limit) return;
+        if(L == n) {
+            if(weightSum>answer) {
+                answer = weightSum;
+            }
+        }
+        else {
+            dfs(L+1, weightSum + dogWeight[L]);
+            dfs(L+1, weightSum);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        limit = sc.nextInt();
+        n = sc.nextInt();
+        dogWeight = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            dogWeight[i] = sc.nextInt();
+        }
+
+        dfs(0,0);
+        System.out.println(answer);
+    }
+}
