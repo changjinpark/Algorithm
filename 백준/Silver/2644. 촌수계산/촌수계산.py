932f9986db9dd1dd2512437acc18a0736deb4cf4
SteadyJin <64358371+changjinpark@users.noreply.github.com>
2022-05-07 22:39:14 +0900

    [Silver II] Title: 촌수계산, Time: 136 ms, Memory: 114276 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -5,25 +5,27 @@ input = sys.stdin.readline
 N = int(input())
 a, b = map(int, input().split())
 m = int(input())
-A = [[0]*(N+1) for _ in range(N+1)]
+graph = [[] for _ in range(N+1)]
 for _ in range(m):
     x, y = map(int, input().split())
-    A[x][y] = A[y][x] = 1
-#print(A)
+    graph[x].append(y)
+    graph[y].append(x)
+
+#print(graph)
+check = [0] * (N+1)
 
 Q = deque()
-Q.append((b, 0))
-visited = []
-visited.append(b)
+Q.append((a, 0))
+check[a] = 1
 
 while Q:
-    cur, cnt = Q.popleft()
-    if cur == a:
+    node, cnt = Q.popleft()
+    if node == b:
         print(cnt)
         break
-    for c in range(1, N+1):
-        if A[cur][c] == 1 and c not in visited:
-            Q.append((c, cnt+1))
-            visited.append(c)
+    for i in graph[node]:
+        if check[i] == 0:
+            check[i] = 1
+            Q.append((i, cnt+1))
 else:
     print(-1)
