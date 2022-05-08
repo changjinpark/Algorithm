bcbd9da3f5addaceb7c348237589cda26f44b302
SteadyJin <64358371+changjinpark@users.noreply.github.com>
2022-05-08 23:27:52 +0900

    [Silver III] Title: N과 M (4), Time: 116 ms, Memory: 21320 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringBuilder sb = new StringBuilder();
+    private static int N;
+    private static int M;
+    private static int[] ans;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        ans = new int[M];
+        //System.out.println(N);
+        //System.out.println(M);
+        solution(0, 1);
+        System.out.println(sb);
+    }
+
+    private static void solution(int L, int start) {
+        if (L == M) {
+            for (int a : ans) {
+                sb.append(a+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i <= N; i++) {
+            ans[L] = i;
+            solution(L+1, i);
+        }
+    }
+}
